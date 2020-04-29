8e4b3861e6bbf0c8ec385a31227489a34ecc42f1
Mike Noel <mike.e.noel3@gmail.com>
2020-04-29 18:52:12 -0500

    pushing up changes to banner

--- client/src/components/banner.js
+++ client/src/components/banner.js
@@ -8,7 +8,7 @@ function Banner(props) {
         <>
         {props.event && Object.entries(props.event).length !== 0 ? 
         <div class="notification is-primary" style={{ position: "sticky", top: "0", zIndex: "1", textAlign: "center" }}>
-            <strong>Now taking orders to be picked up only at {props.event.description} in St. Louis, MO from {moment(props.event.startTime).format("h:mm a")} to {moment(props.event.endTime).format("h:mm a")} on {moment(props.event.endTime).format("dddd, MMMM Do")}</strong> 
+            <strong>Now taking orders to be picked up only at {props.event.description} from {moment(props.event.startTime).format("h:mm a")} to {moment(props.event.endTime).format("h:mm a")} on {moment(props.event.endTime).format("dddd, MMMM Do")}</strong> 
         </div> : null
         }
         </>

bdab46ecf110a95d365762fe77588d80caa7b435
Mike Noel <mike.e.noel3@gmail.com>
2020-07-03 21:20:30 -0500

    pushing up small changes to address deps.

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var port = process.env.PORT || 8080;
 const mongoose = require('mongoose');
 const mongoURI = process.env.MONGODB_URI;
 
-mongoose.connect(mongoURI);
+mongoose.connect(mongoURI, {useNewUrlParser: true, useUnifiedTopology: true});
 mongoose.Promise = global.Promise;
 let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error'))

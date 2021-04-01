78b0ef9f4f8fde378ecf0f15afb9a21f776cda8a
miken91 <49486957+miken91@users.noreply.github.com>
2021-04-01 15:33:51 -0400

    Update checkout.js

--- client/src/pages/checkout.js
+++ client/src/pages/checkout.js
@@ -70,7 +70,7 @@ function Checkout() {
     }
 
     const displayOrderTotal = () => {
-        return ((parseFloat(state.order.orderTotal) * .04) + (parseFloat(state.order.orderTotal * .0873)) + parseFloat(state.order.orderTotal)).toFixed(2)
+        return ((parseFloat(state.order.orderTotal) * .04) + parseFloat(state.order.orderTotal)).toFixed(2)
     }
 
     const event = events.find(event => event._id === orderPlacer.eventId);
